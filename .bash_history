b87902ae610a65ac2aa6e88e098f5c0875bccc92
19PW09@cs2k16.amcs.ws2k12.psgcs.edu <19PW09@cs2k16.amcs.ws2k12.psgcs.edu>
2022-04-29 11:41:42 +0530

    update

---
+++ .bash_history
@@ -0,0 +1,4 @@
+git clone https://github.com/fluttercommunity/plus_plugins.git
+git add .
+git commit -m "niranjan"
+git push
---
+++ Sem6/IoTLab/ws2/fall_detection/android/app/src/main/kotlin/com/example/fall_detection/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fall_detection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
